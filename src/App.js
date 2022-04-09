6e2310f53a8a7c552483e2e9cbd2f97806c81e6f
Capello <godwinkobi25@gmail.com>
2022-04-09 15:23:15 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,47 @@ import HomeTest from './Hometest';
 import StateClassCounter from './StateClassCounter';
 import StatefuctionCounter from './StateFunctionCounter';
 import ClassForm from './ClassForm';
+import { Component } from 'react';
 
-function App() {
-  const name='Akondo'
+class App extends Component {
+ constructor(props){
+   super(props);
+   this.state={
+    user:[
+      {
+      name:"Godwin Dogbe",
+      email:"godwin@gmail.com",
+      gen:"21"
+      },
+      {
+        name:"Priscilla Awuku",
+        email:"priscila@gmail.com",
+        gen:"21"
+        },
+        {
+          name:"Emmanuella Akondo",
+          email:"godwin@gmail.com",
+          gen:"21"
+          },
+  ]
+  }
+
   return (
-   <>
-    {/* <h1 className='cap'>Capello is a good boy</h1>
+      <h1></h1>
+    );
+ }
+  
+}
+
+export default App;
+
+ {/* <>
+      <h1 className='cap'>Capello is a good boy</h1>
    <h3>My name is {name}</h3>
   <Display/>
-  <hr/> */}
+  <hr/> 
   <HomeTest/>
-  <center>
+    <center>
   <h1 className='stateclass'>CLASS STATE</h1>
   <StateClassCounter/>
   <br/>
@@ -23,14 +53,8 @@ function App() {
   <h1 className='stateclass'> FUNCTIONAL STATE</h1>
   <StatefuctionCounter/>
   </center>
-  <hr/>
-  <div className='clsForm'>
+   <hr/> 
+   <div className='clsForm'>
   <ClassForm/>
-  </div>
-  </>
-
- 
-  );
-}
-
-export default App;
+  </div>  
+  </>*/}
